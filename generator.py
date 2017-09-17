aba8cb2e84361c38898c52816bff2e80d0cc3742
Roberto Loja <robertoloja@gmail.com>
2017-09-17 13:19:18 -0400

    Some refactoring and rethinking.

--- generator.py
+++ generator.py
@@ -1,8 +1,17 @@
 import json
 
 resume_content_file = "contents.json"
+print_template = "templates/print_template.html"
+web_template = "templates/web_template.html"
+resume = "resume.html"
 
 with open(resume_content_file) as file:
     content = json.load(file)
 
-print(content)
+with open(print_template) as file:
+    pTemplate = file.read()
+
+print(pTemplate)
+
+# Overwrite/create file
+with open(resume, "w+") as file:
