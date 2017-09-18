57a44ccd5639332806822eda2dd48be2c37f846a
Roberto Loja <robertoloja@gmail.com>
2017-09-18 01:29:25 -0400

    Documentation, refactoring.

--- generate.py
+++ generate.py
@@ -8,34 +8,38 @@ def readHTML(file):
     with open(file) as f:
         return f.readlines()
 
-def renderToFile(file, template, numcols, input):
+
+def renderToFile(file, template, c, JSON):
+    """Write to file, based on template, arranged into c columns, from JSON."""
     with open(file, "w+") as output:
         for line in template:
-            if "<%" in line: #TODO: Turn this into <%-- section -->
-                generateHTML(input, line.split(" ")[1], output, numcols)
+            if "<%--" in line:
+                print(generateHTML(JSON, line.split(" ")[1], c),
+                      file=output)
             else:
                 print(line, file=output)
 
-def generateHTML(input, section, output, numcols):
-    for i in range(numcols):
-        print("<ul class='talent'>", file=output)
-        colsize = int(math.ceil(len(input[section]) / numcols))
 
-        col = [input[section][x] for x in range(i, len(input[section]), numcols)]
+def generateHTML(JSON, section, c):
+    """Return c columns of html, corresponding to section of JSON."""
+    html = ""
+    for i in range(c):
+        html += "<ul class='talent'>"
+        size = int(math.ceil(len(JSON[section]) / c))
+        col = [JSON[section][x] for x in range(i, len(JSON[section]), c)]
 
-        for j in range(colsize - 1):
-            print("<li>" + col[j] + "</li>", file = output)
+        for j in range(size - 1):
+            html += "<li>" + col[j] + "</li>"
 
-        print("<li class='last'>" +
-                (col[-1] if len(col) == colsize else "") + "</li>",
-                file = output)
-        print("</ul>", file = output)
+        html += "<li class='last'>"
+        html += col[-1] if len(col) == size else ""
+        html += "</li></ul>"
+    return html
+
 
 with open("contents.json") as f:
     content = json.load(f)
 
 for medium in ["print", "web"]:
-    renderToFile("resume-" + medium + ".html",
-                 readHTML("templates/" + medium + "_template.html"),
-                 number_of_columns,
-                 content)
+    renderToFile("resume-" + medium + ".html", readHTML("templates/" + medium +
+                 "_template.html"), number_of_columns, content)
