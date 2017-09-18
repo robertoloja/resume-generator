e3c6b76862392d7faa572f7b5ad028520eb8df56
Roberto Loja <robertoloja@gmail.com>
2017-09-18 00:42:42 -0400

    Minor improvements.

--- generate.py
+++ generate.py
@@ -4,27 +4,24 @@ import json
 
 number_of_columns = 3
 
-def openHTML(file):
+def readHTML(file):
     with open(file) as f:
         return f.readlines()
 
-
-def renderToFile(file, template, inputJSON):
+def renderToFile(file, template, numcols, input):
     with open(file, "w+") as output:
         for line in template:
             if "<%" in line: #TODO: Turn this into <%-- section -->
-                generateHTML(inputJSON, line.split(" ")[1], output, number_of_columns)
+                generateHTML(input, line.split(" ")[1], output, numcols)
             else:
                 print(line, file=output)
 
-
-def generateHTML(inputJSON, section, output, numcols):
+def generateHTML(input, section, output, numcols):
     for i in range(numcols):
         print("<ul class='talent'>", file=output)
-        colsize = int(math.ceil(len(inputJSON[section]) / numcols))
+        colsize = int(math.ceil(len(input[section]) / numcols))
 
-        col = [inputJSON[section][x] for x in range(i, len(inputJSON[section]),
-            numcols)]
+        col = [input[section][x] for x in range(i, len(input[section]), numcols)]
 
         for j in range(colsize - 1):
             print("<li>" + col[j] + "</li>", file = output)
@@ -34,12 +31,11 @@ def generateHTML(inputJSON, section, output, numcols):
                 file = output)
         print("</ul>", file = output)
 
-
 with open("contents.json") as f:
     content = json.load(f)
 
-print_template = openHTML("templates/print_template.html")
-web_template = openHTML("templates/web_template.html")
-
-renderToFile("resume-print.html", print_template, content)
-renderToFile("resume-web.html", web_template, content)
+for medium in ["print", "web"]:
+    renderToFile("resume-" + medium + ".html",
+                 readHTML("templates/" + medium + "_template.html"),
+                 number_of_columns,
+                 content)
